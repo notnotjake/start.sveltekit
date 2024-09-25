349683637b120f190a3a304bf85944c932c89785
Jake Go <jake@notnotjake.com>
2024-09-25 17:25:55 -0400

    Installed TailwindCSS
    uses tailwind with PostCSS
    setup in the project
    example usage on the index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1>Start with SvelteKit</h1>
+<div class="flex flex-col gap-2 items-center justify-center py-4">
+	<h1
+		class="font-medium tracking-tight px-4 py-1 rounded-lg transition-all duration-300 hover:text-emerald-500 hover:bg-slate-200 selection:text-emerald-200 selection:bg-indigo-700"
+	>
+		Start with SvelteKit
+	</h1>
+
+	<p class="text-emerald-600">
+		This project is setup with Svelte 5, TypeScript, Prettier, ESLint, Tailwind CSS, Vitetest,
+		Playwright Testing, and More
+	</p>
+</div>
