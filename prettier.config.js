8dc916be47ee12c9ff6b9b2bcf549fa6cf14f4aa
Jake Go <jake@notnotjake.com>
2024-10-29 16:08:24 -0400

    prettier rules
    moves prettierrc to a config.js file and updated the import rules (not totally fixed)

---
+++ prettier.config.js
@@ -0,0 +1,40 @@
+//@ts-check
+
+/** @type {import("prettier").Config} */
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	semi: false,
+	printWidth: 100,
+	plugins: [
+		'prettier-plugin-svelte',
+		'prettier-plugin-tailwindcss',
+		'@ianvs/prettier-plugin-sort-imports'
+	],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+	importOrder: [
+		// Types first
+		'<TYPES>',
+		'^@types/(.*)$',
+		'^type\\s(.*)$',
+		'',
+		// Generic Stuff
+		'<BUILTIN_MODULES>',
+		'<THIRD_PARTY_MODULES>',
+		'',
+		// Svelte core imports
+		'^svelte$',
+		'^svelte/(.*)$',
+		'^@sveltejs/(.*)$',
+		'^\\$app/(.*)$',
+		'^\\$utils/(.*)$',
+		'^\\$env/(.*)$',
+		'',
+		// Local imports with aliases and relative paths
+		'^\\$ui/(.*)$',
+		'^\\$lib/(.*)$',
+		'^[.]'
+	],
+	importParserPlugins: ['typescript', 'jsx', 'decorators-legacy']
+}
