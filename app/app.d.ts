647946494474c2351169993c6b54ce9558728495
Jake Go <jake@notnotjake.com>
2024-10-29 16:12:36 -0400

    svelte hooks for auth
    adds user and session on locals

--- app/app.d.ts
+++ app/app.d.ts
@@ -1,7 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { Session, User } from '$lib/server/db/index'
+
 declare global {
 	namespace App {
+		interface Locals {
+			user: User | null
+			session: Session | null
+		}
+
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
@@ -10,4 +18,4 @@ declare global {
 	}
 }
 
-export {};
+export {}
---
+++ app/hooks/hooks.server.ts
@@ -0,0 +1,30 @@
+import {
+	deleteSessionTokenCookie,
+	getSessionTokenCookie,
+	setSessionTokenCookie
+} from '$lib/server/auth/cookies'
+import { validateSessionToken } from '$lib/server/auth/sessions'
+
+import type { Handle } from '@sveltejs/kit'
+
+export const handle: Handle = async ({ event, resolve }) => {
+	console.log('this code ran')
+
+	const sessionToken = getSessionTokenCookie(event)
+	if (!sessionToken) {
+		event.locals.user = null
+		event.locals.session = null
+		return resolve(event)
+	}
+
+	const { session, user } = await validateSessionToken(sessionToken)
+	if (session !== null) {
+		setSessionTokenCookie(event, sessionToken, session.expiresAt)
+	} else {
+		deleteSessionTokenCookie(event)
+	}
+
+	event.locals.session = session
+	event.locals.user = user
+	return resolve(event)
+}
