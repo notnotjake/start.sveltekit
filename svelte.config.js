0b12f1cf269a4d290b43d21ed8ab77fcc1d26e89
Jake Go <jake@notnotjake.com>
2025-02-04 10:51:07 -0500

    Vercel Adapter with Node version

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs18.x'
+		}),
 		paths: {
 			base: process.env.PUBLIC_URL_BASE || ''
 		},
