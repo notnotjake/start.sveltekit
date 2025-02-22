8131598f475c64bec51f868142491d84d76708d3
Jake Go <jake@notnotjake.com>
2025-02-21 19:25:45 -0500

    A delay function
    Quick implementation. Might be better ways of handling this. Need to do reasearch

--- src/lib/server/auth/utils.ts
+++ src/lib/server/auth/utils.ts
@@ -9,3 +9,17 @@ export function generateToken(byteLength: number = 32): string {
 export function hashToken(token: string): string {
 	return encodeBase64url(sha256(new TextEncoder().encode(token.replace(/=+$/, ''))))
 }
+
+export function setDelay(delayMs: number): number {
+	return Date.now() + delayMs
+}
+
+export async function withDelay<T>(endTime: number, result: T): Promise<T> {
+	const remainingTime = endTime - Date.now()
+
+	if (remainingTime > 0) {
+		await new Promise((resolve) => setTimeout(resolve, remainingTime))
+	}
+
+	return result
+}
