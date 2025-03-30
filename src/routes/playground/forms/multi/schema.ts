9fc71441579c18208fccc9eabed71c3008316be3
Jake Go <jake@notnotjake.com>
2025-03-30 00:36:23 -0400

    Removed unused routes

--- src/routes/playground/forms/multi/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const emailSchema = z.object({
-	email: z.string().email('Email not valid')
-})
-
-export const passwordSchema = z.object({
-	password: z.string().min(8, 'Too Short').max(64, 'Too Long')
-})
