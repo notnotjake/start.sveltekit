349adee34ca22e6bc538cf27e7f680ed06d2705a
Jake Go <jake@notnotjake.com>
2025-04-05 10:00:26 -0400

    Added component: wrap-if
    A new functional component that will wrap child based on a condition.
    This allows you to avoid having to have two separate copies of contents based on if block
    
    Example usage at playground/wrap

---
+++ src/lib/components/containers/wrap-if.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+	import { createClass } from '$utils/create-class'
+
+	// Component Props
+	type Props = {
+		children?: Snippet
+		wrapWith: Snippet<[{ children: Snippet }]>
+		condition: boolean
+		class?: string
+	}
+	let { children, wrapWith, condition, class: classProp }: Props = $props()
+</script>
+
+{#if condition}
+	{@render wrapWith({ children })}
+{:else}
+	{@render children?.()}
+{/if}
