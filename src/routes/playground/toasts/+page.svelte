a5320e829cc68e9b2c678e92f258a4111d17d164
Jake Go <jake@notnotjake.com>
2025-04-23 03:18:25 -0400

    working on new toasts

---
+++ src/routes/playground/toasts/+page.svelte
@@ -0,0 +1,10 @@
+<div class="flex items-center justify-center gap-2 rounded-full bg-neutral-800 px-4 py-2 shadow-sm">
+	<h3 class="font-medium text-white">Logged In</h3>
+	<p class="text-neutral-300">jake@notnotjake.com</p>
+</div>
+
+<div
+	class="flex h-7 cursor-pointer items-center justify-center gap-2 rounded-full bg-neutral-100 px-4 py-2 hover:bg-neutral-200"
+>
+	<p>...</p>
+</div>
