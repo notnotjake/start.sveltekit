75c88dd91f5a2ddd5b46f1f39620b41e5ea3ce60
Jake Go <jake@notnotjake.com>
2025-04-24 00:07:18 -0400

    Updated error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,5 +2,9 @@
 	import { page } from '$app/state'
 </script>
 
-<h1>{page.status}</h1>
-<h2>{page.error.message}</h2>
+<div
+	class="flex min-h-screen w-full flex-col items-center justify-center gap-2 bg-neutral-100 dark:bg-neutral-800"
+>
+	<h1 class="text-4xl font-medium dark:text-white">{page.status}</h1>
+	<h2 class="text-lg text-neutral-700 dark:text-neutral-200">{page.error.message}</h2>
+</div>
