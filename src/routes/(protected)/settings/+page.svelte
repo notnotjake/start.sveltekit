da78d688424de185a21db813584bf78ce5a1870f
Jake Go <jake@notnotjake.com>
2025-04-26 02:27:34 -0400

    Using button on settings page

--- src/routes/(protected)/settings/+page.svelte
+++ src/routes/(protected)/settings/+page.svelte
@@ -12,6 +12,8 @@
 	import Toggle from '$ui/input/toggle.svelte'
 	import LogoutButton from '$ui/auth/logout.svelte'
 
+	import Button from '$ui/input/button.svelte'
+
 	let { data }: PageData = $props()
 
 	function popUpIn(node, { duration = 250, delay = 0, easing = cubicOut }) {
@@ -131,24 +133,18 @@
 							</div>
 						</div>
 
-						<button
-							class="text-vibrant-blue rounded-full border-1 border-transparent bg-transparent px-3.5 py-1.5 text-[0.95rem] font-medium hover:border-blue-50 hover:bg-blue-50"
-							>Edit</button
-						>
+						<Button variant="ghost" href="/settings/change-email">Edit</Button>
 					</div>
 
 					<div
-						class="ring-neutral-150 w-breakout-40 my-3 w-full rounded-2xl bg-neutral-50 px-3 py-3 shadow-xs ring-1"
+						class="ring-neutral-150 w-breakout-40 shadow-card my-3 w-full rounded-2xl bg-white px-3 py-3 ring-1 hover:bg-neutral-50"
 					>
-						<div class="flex items-center justify-between text-[0.95rem] hover:bg-neutral-50">
+						<div class="flex items-center justify-between text-[0.95rem]">
 							<div class="flex w-fit gap-2 text-[0.95rem]">
 								<p class="font-medium">Notifications</p>
 								<p class="text-neutral-700"></p>
 							</div>
-							<button
-								class="border-neutral-150 bg-neutral-150 box-content rounded-full border-1 px-3.5 py-1.5 text-[0.95rem] font-medium text-neutral-800 hover:border-neutral-200 hover:bg-neutral-200"
-								>Manage</button
-							>
+							<Button variant="secondary">Manage</Button>
 						</div>
 					</div>
 
@@ -187,10 +183,7 @@
 								<p class="text-neutral-700">None Setup</p>
 							</div>
 
-							<button
-								class="shadow-primary rounded-full border-1 border-neutral-200 px-3.5 py-1.5 text-[0.9rem] font-medium text-neutral-800 hover:border-neutral-300 hover:bg-neutral-100"
-								>Add Passkey</button
-							>
+							<Button variant="outline">Add Passkey</Button>
 						</div>
 
 						<div class="bg-neutral-150 my-1.5 h-[1px] rounded-full"></div>
@@ -201,10 +194,7 @@
 								<p class="text-neutral-700">Set 2 weeks ago</p>
 							</div>
 
-							<button
-								class="shadow-primary rounded-full border-1 border-neutral-200 px-3.5 py-1.5 text-[0.9rem] font-medium text-neutral-800 hover:border-neutral-300 hover:bg-neutral-100"
-								>Change Password</button
-							>
+							<Button variant="outline" href="/settings/change-password">Change Password</Button>
 						</div>
 					</section>
 
@@ -214,10 +204,7 @@
 								<p class="font-medium">Sessions</p>
 								<p class="text-neutral-700">2 Active</p>
 							</div>
-							<button
-								class="border-neutral-150 bg-neutral-150 box-content rounded-full border-1 px-3.5 py-1.5 text-[0.95rem] font-medium text-neutral-800 hover:border-neutral-200 hover:bg-neutral-200"
-								>Manage</button
-							>
+							<Button variant="secondary">Manage</Button>
 						</div>
 					</div>
 
@@ -237,10 +224,7 @@
 
 					<div class="flex items-center justify-between py-2">
 						<p class="">Delete Account</p>
-						<button
-							class="rounded-full bg-red-500 px-3.5 py-1.5 text-[0.95rem] font-medium text-rose-50 hover:bg-red-600"
-							>Delete Account</button
-						>
+						<Button role="destructive" variant="primary">Delete Account</Button>
 					</div>
 				</div>
 			</Sidebar>
