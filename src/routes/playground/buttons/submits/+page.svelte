da50a9b0c92193f3cbbe7407aeaa7aad635f8c02
Jake Go <jake@notnotjake.com>
2025-04-29 15:44:34 -0400

    New button
    thinking of a ‘superforms’ version of all components

---
+++ src/routes/playground/buttons/submits/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte'
+	import Button from '$ui/input/button.svelte'
+
+	let state: null | 'trying' | 'timeout' | 'success' | 'error' = $state(null)
+
+	function handleClick() {
+		console.log('A')
+	}
+
+	// Another potential feature: countdown also confirm with second click
+</script>
+
+<Button
+	style="outline"
+	onClick={() => {
+		handleClick()
+	}}>Resend</Button
+>
