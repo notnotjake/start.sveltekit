4e06ea5aca2baebd80c8639b35e823a545b1a317
Jake Go <jake@notnotjake.com>
2025-05-21 13:36:45 -0400

    time sheet proto

---
+++ src/routes/0sheet/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	let name = 'Thomas'
+</script>
+
+<div class="min-h-20 w-full bg-neutral-900 px-4 py-8">
+	<div>
+		<h3 class="text-[1.8rem]/8 font-bold text-neutral-300">History</h3>
+	</div>
+
+	<div class="mb-5 flex items-baseline gap-2">
+		<p class="tracking-tight-sm text-[2.1rem]/10 font-bold text-white">Mon</p>
+		<p class="text-[1.4rem]/5 font-bold tracking-tight text-neutral-300">May 19th</p>
+	</div>
+
+	<p class="text-[1.8rem]/8 font-bold text-neutral-300">
+		Lipert Steel <span class="text-[1.2rem]">8am</span>
+	</p>
+	<p class="text-[1.8rem]/8 font-bold text-neutral-300">Hawkins Labs</p>
+	<p class="text-[1.8rem]/8 font-bold text-neutral-300">Choose another job</p>
+</div>
+
+<div class="flex w-full items-center justify-center bg-white py-2 shadow-md/7">
+	<div class="flex h-fit items-center rounded-full bg-neutral-200 px-3.5 py-1.5 inset-shadow-sm">
+		<div class=" px-3.5">
+			<p class=" tracking-tight-sm text-[1.6rem] font-semibold">Today</p>
+		</div>
+
+		<div class="h-9 w-[5px] rounded-full bg-black/15"></div>
+
+		<p class="tracking-tight-sm px-3.5 text-[1.6rem] font-semibold">
+			8:30<span class="text-[1.2rem] opacity-70">AM</span>
+		</p>
+	</div>
+</div>
+
+<div class="h-10"></div>
+
+<div class="flex w-full items-center justify-center bg-white py-2 shadow-md/7">
+	<div class="flex h-fit items-center rounded-full bg-green-500 px-3.5 py-1.5 shadow-sm">
+		<div class=" px-3.5">
+			<p class=" tracking-tight-sm text-[1.6rem] font-semibold text-green-900">Clock In</p>
+		</div>
+
+		<div class="h-9 w-[5px] rounded-full bg-black/15"></div>
+
+		<p class="tracking-tight-sm px-3.5 text-[1.6rem] font-semibold text-green-900">
+			8:30<span class="text-[1.2rem] opacity-70">AM</span>
+		</p>
+	</div>
+</div>
