152fa3db7193d5577cddcb1987a84898a2c37c37
Jake Go <jake@notnotjake.com>
2025-07-07 04:30:29 -0400

    Turso
    dropped the pragma for busy timeout as this was causing vercel error

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -22,7 +22,6 @@ const turso = createClient(connection)
 turso.execute('PRAGMA journal_mode = WAL;') // Better performance
 turso.execute('PRAGMA foreign_keys = ON;') // Enable foreign key constraints
 turso.execute('PRAGMA synchronous = NORMAL;') // Good balance of safety and speed
-turso.execute('PRAGMA busy_timeout = 5000;') // Timeout after 5s
 
 export const db = drizzle({
 	client: turso,
