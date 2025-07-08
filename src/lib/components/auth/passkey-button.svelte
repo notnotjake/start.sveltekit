8bdff61ac49ccf0deb8541bb36bf3f13eab921bf
Jake Go <jake@notnotjake.com>
2025-07-08 02:26:44 -0400

    Error UI for passkey button (at login)

--- src/lib/components/auth/passkey-button.svelte
+++ src/lib/components/auth/passkey-button.svelte
@@ -6,6 +6,8 @@
 	import { Suspense } from '$ui/feedback'
 	import PasskeyIcon from '$ui/icon/passkey.svelte'
 
+	import { IconChevronDown, IconReload, IconInfoTriangleFilled } from '@tabler/icons-svelte'
+
 	import { startAuthentication } from '@simplewebauthn/browser'
 
 	let { identifier, supressAuto = false }: { identifier: string; supressAuto: boolean } = $props()
@@ -18,16 +20,44 @@
 		}
 	})
 
-	async function initiate() {
-		if (isActivating) {
-			isActivating = false
-		} else {
+	async function handleClick() {
+		if (isActivating && hasError.error && hasError.retry) {
 			await getOptions({ auto: false })
+			hasError.error = false
+			hasError.message = null
+		} else if (isActivating && hasError.error && !hasError.retry) {
+			isActivating = false
+			hasError.error = false
+			hasError.message = null
+		} else if (isActivating) {
+			isActivating = false
+		} else if (!isActivating) {
+			await getOptions({ auto: false })
+		}
+	}
+
+	function handleError({ message, retry }: { message: string | null; retry: boolean }) {
+		if (isActivating) {
+			hasError.error = true
+
+			if (message) {
+				hasError.message = message
+				console.log('Passkey Error: ' + message)
+			}
+
+			hasError.retry = retry
+		} else {
+			console.log(
+				message
+					? `Passkey error after user cancelled. ${message}`
+					: 'Passkey error after user cancelled'
+			)
 		}
 	}
 
 	async function getOptions({ auto = false }: { auto: boolean }) {
 		isActivating = true
+
 		try {
 			const response = await fetch('/auth/passkey-authenticate/options', {
 				method: 'POST',
@@ -42,22 +72,24 @@
 			const result = await response.json()
 
 			if (result?.success && result?.data) {
+				console.log('Got Options')
 				startAuth({ optionsJSON: result.data.optionsJSON, useBrowserAutofill: auto })
 			} else {
-				console.error('not verified')
+				handleError({ message: 'Failed to connect to server', retry: true })
 			}
 		} catch (e) {
-			console.error(e)
-			isActivating = false
+			handleError({ message: 'Unexpected issue connecting to server', retry: true })
 		}
 	}
 
 	async function startAuth({ optionsJSON }) {
 		try {
 			const authResponse = await startAuthentication({ optionsJSON })
+			console.log('Started auth')
 			passkeyAuthenticationVerify(authResponse)
 		} catch (e) {
-			console.error(e)
+			console.log(e)
+			// handleError({ message: 'Browser was unable to start passkey authentication', retry: true })
 		}
 	}
 
@@ -74,12 +106,25 @@
 			if (result?.success && result?.redirect) {
 				goto(result.redirect)
 			} else {
-				console.error('not verified')
+				handleError({
+					message: 'Failed to authenticate with passkey. Double check your credentials',
+					retry: true
+				})
 			}
 		} catch (e) {
-			console.error(e)
+			console.log(e)
+			handleError({
+				message: 'Unexpected error trying to authenticate with passkey. Try signing in another way',
+				retry: false
+			})
 		}
 	}
+
+	let hasError = $state({
+		error: false,
+		message: null,
+		retry: false
+	})
 </script>
 
 <div class="relative flex w-full shrink-1 grow basis-1 flex-col items-center">
@@ -96,13 +141,14 @@
 		></div>
 		<button
 			type="button"
-			onclick={initiate}
+			onclick={handleClick}
 			class={createClass(
-				'relative m-auto flex h-fit min-h-12 w-full max-w-full cursor-pointer items-center justify-center gap-2 rounded-[0.9rem] border-none px-4 py-3 font-medium text-white outline-none',
-				isActivating ? 'bg-vibrant-blue grow basis-1 rounded-full' : 'bg-vibrant-blue grow basis-1'
+				'relative m-auto flex h-fit min-h-12 w-full max-w-full cursor-pointer items-center justify-center gap-2 rounded-[0.9rem] px-4 py-3 font-medium text-white outline-none',
+				isActivating ? 'bg-vibrant-blue grow basis-1 rounded-full' : 'bg-vibrant-blue grow basis-1',
+				isActivating && hasError.error && 'border-3 border-rose-500 bg-rose-100 text-rose-500'
 			)}
 		>
-			{#if isActivating}
+			{#if isActivating && !hasError.error}
 				<Suspense.Spinner size={14} thickness={10} speed="fast" tint="var(--color-neutral-100)" />
 				<Suspense.Text
 					class="text-md"
@@ -110,6 +156,13 @@
 					colorBase="var(--color-sky-200)"
 					colorHighlight="var(--color-white)">Trying Passkey</Suspense.Text
 				>
+			{:else if isActivating && hasError.error}
+				<div class="flex">
+					{#if hasError.retry}
+						<IconReload />
+					{/if}
+					<p class="px-2 font-medium whitespace-nowrap">Something went wrong</p>
+				</div>
 			{:else}
 				<PasskeyIcon />
 				<p class="whitespace-nowrap">Use Passkey</p>
@@ -122,6 +175,20 @@
 			)}
 		></div>
 	</div>
+
+	{#if hasError.error && hasError.message !== null}
+		<div class="px-6 pt-8">
+			<div class="flex flex-col items-start justify-start gap-1">
+				<div class="flex items-center gap-1">
+					<IconInfoTriangleFilled size={22} class="text-rose-600" />
+					<p class="font-semibold text-rose-600">Details:</p>
+				</div>
+				<p class="text-neutral-800">
+					{hasError.message}
+				</p>
+			</div>
+		</div>
+	{/if}
 
 	{#if isActivating}
 		<div class="h-5 w-full" transition:wipeVertical></div>
